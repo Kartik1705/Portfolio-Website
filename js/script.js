63abb5a6a01e918991d3f740b83e7bc61afeec6a
Kartik Pandya <kshitijjoshi017@gmail>
2025-10-24 15:34:22 -0400

    Fix: Move keyframes animation from JS to CSS

--- js/script.js
+++ js/script.js
@@ -957,17 +957,6 @@ function loadExperience() {
     });
 }
 
-@keyframes fadeInUp {
-    from {
-        opacity: 0;
-        transform: translateY(30px);
-    }
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-}
-
 // ===== LOAD EDUCATION =====
 function loadEducation() {
     const educationGrid = document.querySelector('.education-grid');
